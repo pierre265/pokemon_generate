d6f62b1bbe2a7359c14ed065361ba9ab467aa658
pierre265 <kevinngako@gmail.com>
2024-07-16 22:41:37 +0200

    Variable check search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,21 @@ import { Pokemon } from "./pokemon";
 })
 export class AppComponent implements OnInit {
   pokemonList: Pokemon [] = POKEMONS;
+  pokemonSelected: Pokemon|undefined;
 
   ngOnInit(): void {
     console.table(this.pokemonList);
   }
 
-  selectPokemon(event: MouseEvent ) {
-    const index: number = +(event.target as HTMLInputElement).value;
-    console.log(`Vous avez cliqué sur le pokémon ${this.pokemonList[index].name}`);
+  selectPokemon(pokemonId: string ) {
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+    if (pokemon) {
+      console.log(`Vous avez demandé le pokémon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    }else{
+      console.log(`Vous avez demandé pokémon non existant`);
+      this.pokemonSelected = pokemon;
+    }
+    
   }
 }
