2076f2d49a65b5da04bd169e433fae0ea98828b0
pierre265 <kevinngako@gmail.com>
2024-07-17 22:17:12 +0200

    gestion de l'erreur 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,16 @@ import { BorderCardDirective } from "./border-card.directive";
 import { DetailPokemonComponent } from "./detail-pokemon/detail-pokemon.component";
 import { ListPokemonComponent } from "./list-pokemon/list-pokemon.component";
 import { PokemonTypeColorPipe } from "./pokemon-type-color.pipe";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 @NgModule({
   declarations: [
-    AppComponent, 
+    AppComponent,
     BorderCardDirective,
     PokemonTypeColorPipe,
     ListPokemonComponent,
     DetailPokemonComponent,
+    PageNotFoundComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
