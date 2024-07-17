4746dffcd29fbff7855f53959d3dec5ffa2ed42f
pierre265 <kevinngako@gmail.com>
2024-07-17 22:06:55 +0200

    redirection of page

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,27 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { POKEMONS } from '../mock-pockemon-list';
-import { Pokemon } from '../pokemon';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { POKEMONS } from "../mock-pockemon-list";
+import { Pokemon } from "../pokemon";
 
 @Component({
-  selector: 'app-detail-pokemon',
-  templateUrl: './detail-pokemon.component.html',
+  selector: "app-detail-pokemon",
+  templateUrl: "./detail-pokemon.component.html",
 })
-
-export class DetailPokemonComponent implements OnInit{
-
+export class DetailPokemonComponent implements OnInit {
   pokemonList: Pokemon[];
-  pokemon: Pokemon|undefined;
+  pokemon: Pokemon | undefined;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
-  ngOnInit()  {
+  ngOnInit() {
     this.pokemonList = POKEMONS;
-    const pokemonId: string|null = this.route.snapshot.paramMap.get('id');
+    const pokemonId: string | null = this.route.snapshot.paramMap.get("id");
 
-    if(pokemonId) {
-      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
-    } 
+    if (pokemonId) {
+      this.pokemon = this.pokemonList.find(
+        (pokemon) => pokemon.id == +pokemonId
+      );
+    }
   }
 
+  goToPokemonList() {
+    this.router.navigate(['/pokemons']);
+  }
 }
