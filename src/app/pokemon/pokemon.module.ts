1f05cde68420fe8fc1e049a80b630af2ce2bcab6
pierre265 <kevinngako@gmail.com>
2024-07-21 09:56:22 +0200

    guard canActivate

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -12,9 +12,10 @@ import { PokemonService } from "./pokemon.service";
 import { AddPokemonComponent } from "./add-pokemon/add-pokemon.component";
 import { SearchPokemonComponent } from "./search-pokemon/search-pokemon.component";
 import { LoaderComponent } from "./loader/loader.component";
+import { AuthGuard } from "../auth.guard";
 
 const pokemonRoutes: Routes = [
-  { path: "edit/pokemon/:id", component: EditPokemonComponent },
+  { path: "edit/pokemon/:id", component: EditPokemonComponent, canActivate: [AuthGuard] },
   { path: "add/pokemon", component: AddPokemonComponent },
   { path: "pokemons", component: ListPokemonComponent },
   { path: "pokemon/:id", component: DetailPokemonComponent },
