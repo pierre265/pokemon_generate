7a7123e272caadb3d9bac77ff80589441b18d57e
pierre265 <kevinngako@gmail.com>
2024-07-21 12:43:49 +0200

    Login and logout system

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -2,20 +2,25 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "../auth.guard";
+import { AddPokemonComponent } from "./add-pokemon/add-pokemon.component";
 import { BorderCardDirective } from "./border-card.directive";
 import { DetailPokemonComponent } from "./detail-pokemon/detail-pokemon.component";
 import { EditPokemonComponent } from "./edit-pokemon/edit-pokemon.component";
 import { ListPokemonComponent } from "./list-pokemon/list-pokemon.component";
+import { LoaderComponent } from "./loader/loader.component";
 import { PokemonFormComponent } from "./pokemon-form/pokemon-form.component";
 import { PokemonTypeColorPipe } from "./pokemon-type-color.pipe";
 import { PokemonService } from "./pokemon.service";
-import { AddPokemonComponent } from "./add-pokemon/add-pokemon.component";
 import { SearchPokemonComponent } from "./search-pokemon/search-pokemon.component";
-import { LoaderComponent } from "./loader/loader.component";
-import { AuthGuard } from "../auth.guard";
+import { LoginComponent } from "../login/login.component";
 
 const pokemonRoutes: Routes = [
-  { path: "edit/pokemon/:id", component: EditPokemonComponent, canActivate: [AuthGuard] },
+  {
+    path: "edit/pokemon/:id",
+    component: EditPokemonComponent,
+    canActivate: [AuthGuard],
+  },
   { path: "add/pokemon", component: AddPokemonComponent },
   { path: "pokemons", component: ListPokemonComponent },
   { path: "pokemon/:id", component: DetailPokemonComponent },
@@ -31,6 +36,7 @@ const pokemonRoutes: Routes = [
     AddPokemonComponent,
     SearchPokemonComponent,
     LoaderComponent,
+    LoginComponent,
   ],
   imports: [CommonModule, FormsModule, RouterModule.forChild(pokemonRoutes)],
   providers: [PokemonService],
