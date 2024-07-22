a93f97c8ddba71e4bf1fec5c70866c409099ff5c
pierre265 <kevinngako@gmail.com>
2024-07-22 13:55:30 +0200

    Login and logout system

--- src/app/pokemon/add-pokemon/add-pokemon.component.ts
+++ src/app/pokemon/add-pokemon/add-pokemon.component.ts
@@ -3,6 +3,7 @@ import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-add-pokemon',
+  templateUrl: './add-pokemon.component.html',
   template: `
     <h2 class="center"> Add Pokemon</h2>
     <app-pokemon-form [pokemon]="pokemon"></app-pokemon-form>
